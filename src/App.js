c49492f72d2df038b659a6f6af57e1904ebcf59a
Damilare <opeyemisamuel222@gmail.com>
2021-01-25 15:16:00 +0000

    selecting a language

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+const App = () => {
+  const [language, setLanguage] = useState("English");
+
+  const onLanguageChange = (language) => {
+    setLanguage(language);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="ui container">
+      <div>
+        Select a Language
+        <i className="flag us" onClick={() => onLanguageChange("English")}></i>
+        <i className="flag nl" onClick={() => onLanguageChange("Dutch")}></i>
+      </div>
+      {language}
     </div>
   );
-}
+};
 
 export default App;
